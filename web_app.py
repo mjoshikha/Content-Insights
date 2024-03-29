81ab2118714c2f681edaab72e75bcba003af6e76
Joshikha M <77103140+mjoshikha@users.noreply.github.com>
2024-03-29 14:27:30 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -36,7 +36,10 @@ def check_relevance_score_with_keywords(meta_description, meta_title, meta_keywo
 
 def extract_content_with_headers(url):
     
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+    'Referer': 'https://www.google.com/',
+    'Origin': 'https://www.google.com/'}
     response = requests.get(url, headers=headers,verify=False)
 
     
