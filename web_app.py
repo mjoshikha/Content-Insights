2aa1de6dcecffba09b6b23d9bd074819effc4b11
Joshikha M <77103140+mjoshikha@users.noreply.github.com>
2024-03-29 17:13:26 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -320,16 +320,12 @@ def main():
     
     elif sidebar_option == "Score Explanation":
         st.sidebar.subheader("Score Explanation")
-        
-        data = {
+        st.sidebar.table({
             "Aspect": ["Content Quality Score","Relevance Score", "Keyword Density Score", "Readability Score"],
             "Green (Favorable)": ["Score is 30","Score >= 8", "Score >= 2", "Score >= 60"],
             "Yellow (Moderate)": ["Score is 20 or 10","6 <= Score < 8", "1 <= Score < 2 ", "50 <= Score < 60"],
             "Red (Poor)": ["Score is 0","Score < 6", "Score < 1", "Score < 50"]
-        }
-
-        df = pd.DataFrame(data)
-        st.sidebar.write(df)
+        })
 
 
 if __name__ == "__main__":
