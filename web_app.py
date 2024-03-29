3e9ebc641cd97c64251d6d718210c19038aa18e8
Joshikha M <77103140+mjoshikha@users.noreply.github.com>
2024-03-29 14:22:00 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -35,7 +35,10 @@ def check_relevance_score_with_keywords(meta_description, meta_title, meta_keywo
     return relevance_score, sorted_word_scores, readability_score
 
 def extract_content_with_headers(url):
-    response = requests.get(url)
+    
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(url, headers=headers,verify=False)
+
     
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
